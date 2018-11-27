0dd716824b0f54711859eb4b0a1d9317c8ca82c2
judhoyos <youam5@hotmail.com>
2018-11-27 07:49:45 -0500

    first commit

---
+++ src/main/java/com/ceiba/ADNParkingCeiba/AdnParkingCeibaApplication.java
@@ -0,0 +1,12 @@
+package com.ceiba.ADNParkingCeiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdnParkingCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdnParkingCeibaApplication.class, args);
+	}
+}
