eba6a50c0ba3de8f533d493cfaeb88417e77c9ed
judhoyos <youam5@hotmail.com>
2018-12-04 16:29:06 -0500

    Creacion de pruebas unitarias

--- src/test/java/com/ceiba/parkingceiba/unit/ControlParqueaderoTest.java
+++ src/test/java/com/ceiba/parkingceiba/unit/ControlParqueaderoTest.java
@@ -1,13 +1,23 @@
 package com.ceiba.parkingceiba.unit;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.ceiba.parkingceiba.domain.IControlParqueadero;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ControlParqueaderoTest {
 
 	private static final String PLACA_INICIA_CON_A = "ASD451";
 	private static final String PLACA_NO_INICIA_CON_A = "BUY987";
+	
+	public boolean respuesta;
 	
 	@Autowired
 	private IControlParqueadero controlParqueadero;
@@ -15,10 +25,11 @@ public class ControlParqueaderoTest {
 	@Test
 	public void validaPlacaIniciaConLetraATest() {
 	//Arrange
-		
+		//Vehiculo vehiculo = new VehiculoTestDataBuilder().conPlaca("ASD451").build();
 	//Act
-		
+		boolean placa = controlParqueadero.validarPlacaIniciaPorLetraA(PLACA_INICIA_CON_A);
 	//Assert
+		assertTrue(placa);
 	}
 	
 	@Test
@@ -26,7 +37,7 @@ public class ControlParqueaderoTest {
 	//Arrange
 		
 	//Act
-		
+
 	//Assert
 
 	}
