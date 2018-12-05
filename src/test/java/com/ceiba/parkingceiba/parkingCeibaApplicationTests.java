1ac0a7a68590abb7aa7050da2446a749d715e337
judhoyos <youam5@hotmail.com>
2018-12-05 08:40:02 -0500

    Se devuelve metodo a clase parkingCeibaApplicationTests

--- src/test/java/com/ceiba/parkingceiba/parkingCeibaApplicationTests.java
+++ src/test/java/com/ceiba/parkingceiba/parkingCeibaApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class parkingCeibaApplicationTests {
 
-	/*@Test
+	@Test
 	public void contextLoads() {
-	}*/
+	}
 
 }
