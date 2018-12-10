e0ea0bd3d08cbc52f8bf5b3f0e48cbe99b09ab13
judhoyos <youam5@hotmail.com>
2018-12-10 14:28:13 -0500

    Pruebas de Integracion

--- src/test/java/com/ceiba/parkingceiba/integracion/ControlParqueaderoService.java
+++ src/test/java/com/ceiba/parkingceiba/integracion/ControlParqueaderoService.java
@@ -25,7 +25,7 @@ public class ControlParqueaderoService {
 
 	private static final int CILINDRAJE = 180;
 
-	//private static final String TIPO_VEHICULO_AVION = "Avion";
+	// private static final String TIPO_VEHICULO_AVION = "Avion";
 
 	private static final String PLACA = "XCD123";
 
@@ -40,20 +40,22 @@ public class ControlParqueaderoService {
 				.conTipoVehiculo(EnumTipoVehiculo.MOTO).build();
 		ResponseEntity<Parqueadero> parqueadero = restTemplate.postForEntity(
 				"http://localhost:" + localServerPort + "/parqueadero/registroEntrada", vehiculo, Parqueadero.class);
-		//System.out.println("Puerto: " + localServerPort);
+		// System.out.println("Puerto: " + localServerPort);
 		assertEquals(HttpStatus.CREATED, parqueadero.getStatusCode());
 	}
 
-	/*
-	 * @Test public void registrarIngresoFallido() { Vehiculo vehiculo = new
-	 * VehiculoTestDataBuilder().conPlaca(PLACA).conCilindraje(CILINDRAJE).
-	 * conTipoVehiculo(TIPO_VEHICULO_AVION).build(); ResponseEntity<String>
-	 * parqueadero = restTemplate.postForEntity("http://localhost:"+localServerPort+
-	 * "/parqueadero/registroEntrada", vehiculo,String.class);
-	 * System.out.println("Puerto: " + localServerPort);
-	 * assertEquals(CatalogoMensajes.INGRESO_VEHICULO_DIFERENTE_A_CARRO_O_MOTO,
-	 * parqueadero.getBody());
-	 * 
-	 * }
-	 */
+	/*@Test
+	public void registroSalida() {
+		ResponseEntity<Parqueadero> parqueadero = restTemplate.postForEntity(
+				"http://localhost:" + localServerPort + "/parqueadero/registroSalida", PLACA, Parqueadero.class);
+		System.out.println("Puerto: " + localServerPort);
+		assertEquals(HttpStatus.OK, parqueadero.getStatusCode());
+	}*/
+	
+	@Test
+	public void buscarTodosLosVehiculos() {
+	ResponseEntity<String> parqueadero = restTemplate.getForEntity("http://localhost:"+localServerPort+"/parqueadero/buscarVehiculos",String.class);
+	System.out.println("Puerto: " + localServerPort);
+	assertEquals(HttpStatus.NOT_ACCEPTABLE, parqueadero.getStatusCode());
+	}
 }
